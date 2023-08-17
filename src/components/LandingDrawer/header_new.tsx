06fbd1e82cc4f0206b558ad148535b05167b7697
adityamo <rfadityahalim18@gmail.com>
2023-08-17 20:58:01 +0700

    last_update

---
+++ src/components/LandingDrawer/header_new.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const header = () => {
+  return (
+    <header
+      id="header"
+      className="header fixed-top d-flex align-items-center"
+    ></header>
+  );
+};
+
+export default header;
